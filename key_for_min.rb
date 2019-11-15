6246984710a34e932334637e6f7ebfa9e335cce0
vy vo <tuonggvy.vo@gmail.com>
2019-11-15 03:59:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  
+  lowest_value = 0
+  lowest_key = nil
   name_hash.collect do |key, value|
-    value
+    if lowest_value == 0 || value < lowest_value
+      lowest_key = key
   end
 end
